f5924af050b33cedf2bca008e6ff951bb6aa9483
wifi-left <ysy1679682327@outlook.com>
2026-03-17 20:31:16 +0800

    6

--- src/main/java/sir/nicholascooke/watheextraitems/item/DrinkableItem.java
+++ src/main/java/sir/nicholascooke/watheextraitems/item/DrinkableItem.java
@@ -1,10 +1,14 @@
 package sir.nicholascooke.watheextraitems.item;
 
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.HoneyBottleItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
 
 public class DrinkableItem extends HoneyBottleItem {
 
@@ -15,6 +19,12 @@ public class DrinkableItem extends HoneyBottleItem {
     @Override
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.DRINK;
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        FoodComponent foodProperties = stack.get(DataComponentTypes.FOOD);
+        return foodProperties != null ? user.eatFood(world, stack, foodProperties) : stack;
     }
 
     @Override
